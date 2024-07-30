026a395a62ecd239b0826059fe15fb76977136f7
Simon Thore <simon.thore@hotmail.fr>
2024-07-30 11:18:27 +0200

    mise en place de la fonction de recherche

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -9,7 +9,6 @@ function App() {
     return (
         <Router>
             <Navbar />
-            {/* test */}
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/Champion" element={<Champion />} />
--- front/src/screens/Home.tsx
+++ front/src/screens/Home.tsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import magnifyingGlass from "../assets/magnifying.svg";
 
 export default function Home() {
-    // const [searchInput, setSearchInput] = useState("");
+    const [searchInput, setSearchInput] = useState("");
+
+    function handleChange(event) {
+        setSearchInput(event.target.value);
+    }
+
     return (
-        <div>
+        <div className="general">
             <h1>Home</h1>
             <p>Welcome to the LeagueStats</p>
-            <input
-                type="text"
-                // value={searchInput}
-                // onChange={handleChange}
-                placeholder="Nom du joueur"
-            />
+            <div className="search-area">
+                <nav>
+                    <ul className="menu">
+                        <li className="deroulant">
+                            <a href="#">Region</a>
+                            <ul className="sous">
+                                <li><a href="#">Eu West</a></li>
+                                <li><a href="#">Eu Est</a></li>
+                                <li><a href="#">USA</a></li>
+                                <li><a href="#">Asia</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+                <input
+                    type="text"
+                    className="main-search"
+                    value={searchInput}
+                    onChange={handleChange}
+                    placeholder="Nom du joueur"
+                />
+                <div className="button-container">
+                    <button className="button-search">
+                        <img src={magnifyingGlass} alt="magnifyingGlass" />
+                        {/* onChange ={(e)  =>{
+                            setSearchPlayer(e.target.value)
+                        }} */}
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
