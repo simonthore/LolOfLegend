416089051dab40387957b9c988a66c7f610f2f8f
Simon Thore <simon.thore@hotmail.fr>
2024-09-09 08:01:15 +0200

    transfert vers pc fix

--- front/src/screens/Profile.tsx
+++ front/src/screens/Profile.tsx
@@ -9,6 +9,8 @@ interface Summoner {
     tagLine: string;
     profileIconId?: number;
     encryptedSummonerId?: string;  // Nouveau champ pour l'ID chiffré
+    rankingSolo?: string;  // Nouveau champ pour le classement en solo
+    rankingFlex?: string;  // Nouveau champ pour le classement en flex
 }
 
 export default function SummonerProfile() {
@@ -25,6 +27,8 @@ export default function SummonerProfile() {
                 try {
                     // Requête initiale pour récupérer le puuid
                     const initialUrl = `${API_BASE_URL}/api/summoner/${summonerName}/${region}`;
+                    console.log(initialUrl, 'initialUrl');
+                    
                     const initialResponse = await axios.get(initialUrl);
                     const initialData = initialResponse.data;
 
@@ -32,11 +36,28 @@ export default function SummonerProfile() {
                         const puuid = initialData.puuid;
                         // Nouvelle requête pour récupérer des infos supplémentaires via puuid
                         const summonerUrl = `https://${region}1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${RIOT_API_KEY}`;
-                        console.log(RIOT_API_KEY, 'apiKey');
+                        console.log(summonerUrl, 'summonerUrl');
                         
                         const summonerResponse = await axios.get(summonerUrl);
                         const summonerData = summonerResponse.data;
+                        const summonerLeagueUrl = `https://${region}1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerData.id}?api_key=${RIOT_API_KEY}`;
+                        const summonerLeagueResponse = await axios.get(summonerLeagueUrl);
+                        console.log(summonerLeagueResponse.data , 'summonerLeagueResponse');
 
+                        if (summonerLeagueResponse.data.length > 0) {
+                            summonerData.tierSolo = summonerLeagueResponse.data[0].tier;
+                            summonerData.rankSolo = summonerLeagueResponse.data[0].rank;
+                            summonerData.tierFlex = summonerLeagueResponse.data[2].tier;
+                            summonerData.rankFlex = summonerLeagueResponse.data[2].rank;
+                        } 
+                        else {
+                            summonerData.tier = 'UNRANKED';
+                            summonerData.rank = '';
+                        }
+                        
+                       
+                        
+                        
                         const summoner: Summoner = {
                             name: summonerData.name,
                             level: summonerData.summonerLevel,
@@ -44,6 +65,8 @@ export default function SummonerProfile() {
                             tagLine: initialData.tagLine, // On garde le tagLine récupéré dans la première requête
                             profileIconId: summonerData.profileIconId,
                             encryptedSummonerId: summonerData.id,  // Récupération de l'ID chiffré
+                            rankingSolo: summonerData.tierSolo + ' ' + summonerData.rankSolo,
+                            rankingFlex: summonerData.tierFlex + ' ' + summonerData.rankFlex
                         };
                         setSummonerData(summoner);
                         setError(null);
@@ -53,7 +76,7 @@ export default function SummonerProfile() {
                     }
                 } catch (error) {
                     console.error("An error occurred while fetching the summoner data:", error);
-                    setError("An error occurred while fetching the summoner data.");
+                    setError(`An error occurred: ${error.response ? error.response.data.message : error.message}`);
                     setSummonerData(null);
                 }
             }
@@ -62,6 +85,8 @@ export default function SummonerProfile() {
         fetchSummonerData();
     }, [summonerName, region]);
 
+
+    
     return (
         <div className="main-profile">
             <h1>Summoner Profile</h1>
@@ -79,6 +104,8 @@ export default function SummonerProfile() {
                     <h2>name :{summonerName}</h2>
                     <h3>{summonerData.tagLine}</h3>
                     <p>PUUID: {summonerData.puuid}</p>
+                    <p>summoner rank Solo: {summonerData.rankingSolo}</p>
+                    <p>summoner rank Flex: {summonerData.rankingFlex}</p>
                     <p>Encrypted Summoner ID: {summonerData.encryptedSummonerId}</p> {/* Affichage de l'ID chiffré */}
                 </div>
             ) : (
